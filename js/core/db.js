eb5c27f1ca7191994766125c43fe37137c4fd368
nethanbenogie-code <nethanbenogie@gmail.com>
2026-05-29 10:50:06 +0800

    Update db.js

--- js/core/db.js
+++ js/core/db.js
@@ -1,30 +1,16 @@
-// js/core/db.js – Simplified localStorage-only version
+// js/core/db.js
 import { LocalDB } from './localDB.js';
 
 export const DB = {
-  async getAll(store) {
-    return LocalDB.getAll(store);
-  },
-  async set(store, data) {
-    LocalDB.set(store, data);
-  },
-  async add(store, item) {
-    return LocalDB.add(store, item);
-  },
-  async update(store, item) {
-    LocalDB.update(store, item);
-  },
-  async delete(store, id) {
-    LocalDB.delete(store, id);
-  },
-  async getById(store, id) {
-    return LocalDB.getById(store, id);
-  },
-  async getByBranch(store, bid) {
-    return LocalDB.getByBranch(store, bid);
-  }
+  async getAll(store) { return LocalDB.getAll(store); },
+  async set(store, data) { LocalDB.set(store, data); },
+  async add(store, item) { return LocalDB.add(store, item); },
+  async update(store, item) { LocalDB.update(store, item); },
+  async delete(store, id) { LocalDB.delete(store, id); },
+  async getById(store, id) { return LocalDB.getById(store, id); },
+  async getByBranch(store, bid) { return LocalDB.getByBranch(store, bid); }
 };
 
 export async function initDB() {
-  console.log('Storage mode: localStorage (simplified)');
+  console.log('Storage mode: localStorage');
 }
