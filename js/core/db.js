ac59595b8a2d6583195524442367c104b39f85fa
nethanbenogie-code <nethanbenogie@gmail.com>
2026-05-29 10:04:51 +0800

    Update db.js

--- js/core/db.js
+++ js/core/db.js
@@ -59,8 +59,4 @@ export async function initDB() {
     console.error('IDB init failed, falling back to localStorage', err);
     storageMode = 'local';
   }
-  // Ensure settings store exists in localStorage
-  if (!localStorage.getItem('mlea_settings')) {
-    localStorage.setItem('mlea_settings', '[]');
-  }
 }
